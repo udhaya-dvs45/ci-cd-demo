dd624094b8beee4663c53e7443047e010bb36843
udhaya-dvs45 <udhayakiran100@gmail.com>
2026-03-18 15:56:26 +0530

    new

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
-def add(a, b):
-    return a + b
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "CI/CD is working 🚀"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+    
